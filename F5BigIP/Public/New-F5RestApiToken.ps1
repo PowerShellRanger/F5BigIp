84b4650120cf361fffcd391a4f1e66f765eb4e94
Travis Allen <tallen@thinkfinance.com>
2018-04-06 14:52:43 -0500

    initial commit

---
+++ F5BigIP/Public/New-F5RestApiToken.ps1
@@ -0,0 +1,66 @@
+function New-F5RestApiToken
+{
+    <#
+    .Synopsis
+        Generate a token for BigIP Rest API access
+    .DESCRIPTION
+        Generate a token for authenticating to BigIP F5 via the REST API interface.
+    .EXAMPLE
+        New-F5RestApiToken -F5Name 'myF5.mydomain.com' -Credential (Get-Credential MyUser) -ErrorAction Stop -Verbose
+    .EXAMPLE
+       
+    #>
+    [CmdletBinding(SupportsShouldprocess, ConfirmImpact = "High")]
+    param
+    (
+        # F5Name
+        [Parameter(
+            Mandatory, 
+            ValueFromPipeline, 
+            ValueFromPipelineByPropertyName
+        )]        
+        [string]$F5Name,
+
+        # Credentials to F5
+        [Parameter(
+            Mandatory, 
+            ValueFromPipeline, 
+            ValueFromPipelineByPropertyName
+        )]
+        [PSCredential]$Credential
+    )
+    begin
+    {
+    }
+    process
+    {
+        if ($PSCmdlet.Shouldprocess("Generate Rest API Token for User: $($Credential.UserName) on F5: $F5Name"))
+        {
+            $psObjectBody = [PSCustomObject] @{
+                username          = $($Credential.UserName)
+                password          = $($Credential.GetNetworkCredential().Password)
+                loginProviderName = "tmos"
+            }
+            $body = $psobjectBody | ConvertTo-Json
+
+            Write-Verbose "Starting Invoke-WebRequest: $F5Name to generate a token for $($Credential.UserName)."
+            $splatInvokeRestMethod = @{
+                Uri         = "https://$F5Name/mgmt/shared/authn/login"
+                ContentType = 'application/json'
+                Method      = 'POST'
+                Body        = $body
+                ErrorAction = 'Stop'
+            }
+            $response = Invoke-RestMethod @splatInvokeRestMethod
+            
+            [PSCustomObject] @{
+                UserName = $response.token.userName
+                Token    = $response.token.token
+            }
+        }
+    }
+    end
+    {
+    }
+}
+
