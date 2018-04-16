60191db2201d28b2a7ec7743de46e3489a4e107e
Jason Wainwright <jwainwright@thinkfinance.com>
2018-04-16 16:05:45 -0500

    Added Get-F5ClientSslProfile and Get-F5VirtualServer

---
+++ F5BigIP/Public/Get-F5ClientSslProfile.ps1
@@ -0,0 +1,96 @@
+function Get-F5ClientSslProfile
+{
+    <#
+    .Synopsis
+       
+    .DESCRIPTION
+       
+    .EXAMPLE
+       
+    .EXAMPLE
+       
+    #>
+    [CmdletBinding(
+        DefaultParameterSetName = 'OnlyGetClientSslProfilesRequested'
+    )]
+    param
+    (
+        # F5Name
+        [Parameter(
+            Mandatory, 
+            ValueFromPipeline, 
+            ValueFromPipelineByPropertyName,
+            ParameterSetName = 'OnlyGetClientSslProfilesRequested'
+        )]
+        [Parameter(
+            Mandatory, 
+            ValueFromPipeline, 
+            ValueFromPipelineByPropertyName,
+            ParameterSetName = 'GetAllClientSslProfiles'
+        )]
+        [string]$F5Name,
+
+        # Token Based Authentication
+        [Parameter(
+            Mandatory, 
+            ValueFromPipeline, 
+            ValueFromPipelineByPropertyName,
+            ParameterSetName = 'OnlyGetClientSslProfilesRequested'
+        )]
+        [Parameter(
+            Mandatory, 
+            ValueFromPipeline, 
+            ValueFromPipelineByPropertyName,
+            ParameterSetName = 'GetAllClientSslProfiles'
+        )]
+        [string]$Token,
+
+        # Name of Certificates to get
+        [Parameter(
+            ParameterSetName = 'OnlyGetClientSslProfilesRequested'
+        )]
+        [string[]]$ClientSslProfileName,
+        
+        # Switch to get all Certificates
+        [Parameter(            
+            ValueFromPipeline, 
+            ValueFromPipelineByPropertyName,
+            ParameterSetName = 'GetAllClientSslProfiles'
+        )]
+        [switch]$GetAllClientSslProfiles
+    )
+    begin
+    {
+    }
+    process
+    {
+        $errorAction = $ErrorActionPreference        
+        if ($PSBoundParameters["ErrorAction"])
+        {
+            $errorAction = $PSBoundParameters["ErrorAction"]
+        }
+
+        $headers = @{
+            'X-F5-Auth-Token' = $Token
+        }
+        if ($PSBoundParameters['GetAllClientSslProfiles'])
+        {
+            $url = "https://$F5Name/mgmt/tm/ltm/profile/client-ssl"
+            Write-Verbose "Invoke Rest Method to: $url"
+            (Invoke-RestMethod -Method Get -Uri $url -Headers $headers -ContentType "application/json" -ErrorAction $errorAction).items
+        }
+        else
+        {
+            foreach ($ClientSslProfile in $ClientSslProfileName)
+            {                
+                $url = "https://$F5Name/mgmt/tm/ltm/profile/client-ssl/~Common~$ClientSslProfile"
+                Write-Verbose "Invoke Rest Method to: $url"
+                Invoke-RestMethod -Method Get -Uri $url -Headers $headers -ContentType "application/json" -ErrorAction $errorAction
+            }
+        }        
+    }
+    end
+    {
+    }
+}
+
---
+++ F5BigIP/Public/Get-F5VirtualServer.ps1
@@ -0,0 +1,96 @@
+function Get-F5VirtualServer
+{
+    <#
+    .Synopsis
+       
+    .DESCRIPTION
+       
+    .EXAMPLE
+       
+    .EXAMPLE
+       
+    #>
+    [CmdletBinding(
+        DefaultParameterSetName = 'OnlyGetVirtualServersRequested'
+    )]
+    param
+    (
+        # F5Name
+        [Parameter(
+            Mandatory, 
+            ValueFromPipeline, 
+            ValueFromPipelineByPropertyName,
+            ParameterSetName = 'OnlyGetVirtualServersRequested'
+        )]
+        [Parameter(
+            Mandatory, 
+            ValueFromPipeline, 
+            ValueFromPipelineByPropertyName,
+            ParameterSetName = 'GetAllVirtualServers'
+        )]
+        [string]$F5Name,
+
+        # Token Based Authentication
+        [Parameter(
+            Mandatory, 
+            ValueFromPipeline, 
+            ValueFromPipelineByPropertyName,
+            ParameterSetName = 'OnlyGetVirtualServersRequested'
+        )]
+        [Parameter(
+            Mandatory, 
+            ValueFromPipeline, 
+            ValueFromPipelineByPropertyName,
+            ParameterSetName = 'GetAllVirtualServers'
+        )]
+        [string]$Token,
+
+        # Name of Certificates to get
+        [Parameter(
+            ParameterSetName = 'OnlyGetVirtualServersRequested'
+        )]
+        [string[]]$VirtualServerName,
+        
+        # Switch to get all Certificates
+        [Parameter(            
+            ValueFromPipeline, 
+            ValueFromPipelineByPropertyName,
+            ParameterSetName = 'GetAllVirtualServers'
+        )]
+        [switch]$GetAllVirtualServers
+    )
+    begin
+    {
+    }
+    process
+    {
+        $errorAction = $ErrorActionPreference        
+        if ($PSBoundParameters["ErrorAction"])
+        {
+            $errorAction = $PSBoundParameters["ErrorAction"]
+        }
+
+        $headers = @{
+            'X-F5-Auth-Token' = $Token
+        }
+        if ($PSBoundParameters['GetAllVirtualServers'])
+        {
+            $url = "https://$F5Name/mgmt/tm/ltm/virtual"
+            Write-Verbose "Invoke Rest Method to: $url"
+            (Invoke-RestMethod -Method Get -Uri $url -Headers $headers -ContentType "application/json" -ErrorAction $errorAction).items
+        }
+        else
+        {
+            foreach ($VirtualServer in $VirtualServerName)
+            {                
+                $url = "https://$F5Name/mgmt/tm/ltm/virtual/~Common~$VirtualServer"
+                Write-Verbose "Invoke Rest Method to: $url"
+                Invoke-RestMethod -Method Get -Uri $url -Headers $headers -ContentType "application/json" -ErrorAction $errorAction
+            }
+        }        
+    }
+    end
+    {
+    }
+}
+
