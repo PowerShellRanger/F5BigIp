309931a1382b6172405db4994277b3de69be630b
Travis Allen <tallen@thinkfinance.com>
2018-04-16 14:26:01 -0500

    changed Jason's test back

--- Tests/F5BigIP.tests.ps1
+++ Tests/F5BigIP.tests.ps1
@@ -7,7 +7,7 @@ Describe "General project validation: $moduleName" {
 
     $scripts = Get-ChildItem $moduleRoot -Include *.ps1, *.psm1, *.psd1 -Recurse
 
-    # TestCases are splatted to the script so we need hashtables test
+    # TestCases are splatted to the script so we need hashtables
     $testCase = $scripts | Foreach-Object {@{file = $_}}         
     It "Script <file> should be valid powershell" -TestCases $testCase {
         param($file)
