c33723ca1f448f22730495e00fe6b2aa0677b628
Jason Wainwright <jwainwright@thinkfinance.com>
2018-04-23 14:51:15 -0500

    Trimmming unused parameters

--- Tests/New-F5Pool.Tests.ps1
+++ Tests/New-F5Pool.Tests.ps1
@@ -71,8 +71,6 @@ InModuleScope -ModuleName $moduleName {
                 Assert-MockCalled -CommandName Invoke-RestMethod -Times 1 -ParameterFilter {$Uri -eq "https://$F5Name/mgmt/tm/ltm/pool"}
                 Assert-MockCalled -CommandName Invoke-RestMethod -Times 1 -ParameterFilter {$ContentType -eq 'application/json'}
                 Assert-MockCalled -CommandName Invoke-RestMethod -Times 1 -ParameterFilter {$Method -eq 'Post'}
-                Assert-MockCalled -CommandName Invoke-RestMethod -Times 1 
-                Assert-MockCalled -CommandName Invoke-RestMethod -Times 1 
             }
         }
         
