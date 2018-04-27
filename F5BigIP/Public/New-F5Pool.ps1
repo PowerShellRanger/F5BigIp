d136cc40e531c79a2636c276d27d6f40e75b5fbc
Jason Wainwright <jwainwright@thinkfinance.com>
2018-04-27 16:28:19 -0500

    removed dynaic param

--- F5BigIP/Public/New-F5Pool.ps1
+++ F5BigIP/Public/New-F5Pool.ps1
@@ -40,25 +40,6 @@ function New-F5Pool
         )]        
         $F5Pool
     )
-   
-    DynamicParam
-    {
-        if ($Monitor -eq 'Custom')
-        {
-            Write-Verbose 'Create a new ParameterAttribute Object.'
-            $eventSourceNameAttribute = New-Object System.Management.Automation.ParameterAttribute
-            $eventSourceNameAttribute.Mandatory = $true
-            $eventSourceNameAttribute.ValueFromPipeline = $true
-            $eventSourceNameAttribute.ValueFromPipelineByPropertyName = $true
-            Write-Verbose 'Create an attributecollection object for the attribute just created.'
-            $attributeCollection = New-Object System.Collections.ObjectModel.Collection[System.Attribute]
-            $attributeCollection.Add($eventSourceNameAttribute)
-            $customMonitorNameParam = New-Object System.Management.Automation.RuntimeDefinedParameter('CustomMonitorName', [string], $attributeCollection)
-            $paramDictionary = New-Object System.Management.Automation.RuntimeDefinedParameterDictionary
-            $paramDictionary.Add('CustomMonitorName', $customMonitorNameParam)
-            return $paramDictionary
-        }
-    }
     begin
     {
     }
