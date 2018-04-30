accc632c5025e8393eab926a9135e588782ee852
Travis Allen <tallen@thinkfinance.com>
2018-04-30 12:56:46 -0500

    changed type on IPAddress param

--- F5BigIP/Public/New-F5Node.ps1
+++ F5BigIP/Public/New-F5Node.ps1
@@ -47,7 +47,7 @@ function New-F5Node
             ValueFromPipelineByPropertyName
         )]        
         [ValidatePattern("\A(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\z")]
-        [IAddress]$IpV4Address
+        [string]$IpV4Address
     )
     begin
     {
