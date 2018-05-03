5eac878a63b43d0165ee56c46db95c9e286da2d1
Travis Allen <tallen@thinkfinance.com>
2018-05-03 17:50:04 -0500

    removed member class

--- F5BigIP/Classes/004-F5Pool.ps1
+++ F5BigIP/Classes/004-F5Pool.ps1
@@ -50,36 +50,3 @@ class F5Pool
         return  "/Common/" + $monitorName      
     }    
 }
-
-class F5Member
-{
-    # Member Name 
-    [string]$Name
-
-    # Source Address
-    [string]$Address
-
-    # Service Port
-    [ValidateSet('HTTP', 'HTTPS')]
-    [string]$ServicePort = "HTTPS"
-
-    F5Member ([string]$name, [IpAddress]$ip)
-    {
-        $this.Name = [F5Member]::GetMemberName($name, $this.ServicePort)
-        $this.Address = $ip.IpAddress
-    }
-
-    F5Member ([string]$name, [IpAddress]$ip, [string]$servicePort)
-    {
-        $this.Name = [F5Member]::GetMemberName($name, $servicePort)
-        $this.Address = $ip.IpAddress
-        $this.ServicePort = $servicePort
-    }
-
-    static [string] GetMemberName([string]$name, [string]$servicePort)
-    {                
-        if ($servicePort -eq 'HTTP') {return "$($name):80"}
-        
-        return "$($name):443"
-    }    
-}
