1e47217b67833107dc89acbf7957730b659a9217
Travis Allen <tallen@thinkfinance.com>
2018-05-24 17:49:52 -0500

    added output types and fixed caseing

--- F5BigIP/Public/Get-F5Node.ps1
+++ F5BigIP/Public/Get-F5Node.ps1
@@ -10,7 +10,7 @@ function Get-F5Node
     .EXAMPLE
        
     #>
-    [OutputType('F5Node', 'F5Node[]')]
+    [OutputType('F5Node')]
     [CmdletBinding(
         DefaultParameterSetName = 'OnlyGetNodesRequested'
     )]
@@ -18,6 +18,8 @@ function Get-F5Node
     (
         # Name of Nodes to get
         [Parameter(
+            ValueFromPipeline, 
+            ValueFromPipelineByPropertyName,
             ParameterSetName = 'OnlyGetNodesRequested'
         )]
         [string[]]$NodeName,
@@ -48,10 +50,10 @@ function Get-F5Node
         }
         else
         {
-            foreach ($Node in $NodeName)
+            foreach ($node in $NodeName)
             {                
-                Write-Verbose "Invoke Rest Method to: https://$($Script:F5Session.F5Name)/mgmt/tm/ltm/node/~Common~$NodeName"                
-                [F5Node]::Get($nodeName, $Script:F5Session)
+                Write-Verbose "Invoke Rest Method to: https://$($Script:F5Session.F5Name)/mgmt/tm/ltm/node/~Common~$node"
+                [F5Node]::Get($node, $Script:F5Session)
             }
         }        
     }
--- F5BigIP/Public/Get-F5Pool.ps1
+++ F5BigIP/Public/Get-F5Pool.ps1
@@ -10,6 +10,7 @@ function Get-F5Pool
     .EXAMPLE
        
     #>
+    [OutputType('F5Pool')]
     [CmdletBinding(
         DefaultParameterSetName = 'OnlyGetPoolsRequested'
     )]
@@ -39,22 +40,18 @@ function Get-F5Pool
         {
             $errorAction = $PSBoundParameters["ErrorAction"]
         }
-
-        $headers = @{
-            'X-F5-Auth-Token' = $Token
-        }
+        
         if ($PSBoundParameters['GetAllPools'])
         {
             Write-Verbose "Invoke Rest Method to: https://$($Script:F5Session.F5Name)/mgmt/tm/ltm/pool"
             [F5Pool]::GetAllNodes($Script:F5Session)
-
         }
         else
         {
-            foreach ($Pool in $PoolName)
+            foreach ($pool in $PoolName)
             {                
-                Write-Verbose "Invoke Rest Method to: https://$($Script:F5Session.F5Name)/mgmt/tm/ltm/pool/~Common~$Pool"
-                [F5Pool]::Get($PoolName,$Script:F5Session)
+                Write-Verbose "Invoke Rest Method to: https://$($Script:F5Session.F5Name)/mgmt/tm/ltm/pool/~Common~$pool"
+                [F5Pool]::Get($pool, $Script:F5Session)
             }
         }        
     }
