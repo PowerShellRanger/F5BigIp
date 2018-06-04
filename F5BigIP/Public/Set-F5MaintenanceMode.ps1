57b5640249749378f355fc82360e803e5767c482
Travis Allen <tallen@thinkfinance.com>
2018-06-04 17:43:08 -0500

    added code. I guess I commited last time without anything

---
+++ F5BigIP/Public/Set-F5MaintenanceMode.ps1
@@ -0,0 +1,73 @@
+function Set-F5MaintenanceMode
+{
+    <#
+    .Synopsis
+       
+    .DESCRIPTION
+       
+    .EXAMPLE
+       
+    .EXAMPLE
+       
+    #>
+    [CmdletBinding(
+        SupportsShouldProcess, 
+        ConfirmImpact = "High"
+    )]
+    param
+    (
+        # Name of iRule
+        [Parameter(
+            ValueFromPipeline, 
+            ValueFromPipelineByPropertyName           
+        )]
+        [string[]]$Name,
+
+        # MaintenanceMode On/Off
+        [Parameter(
+            ValueFromPipeline, 
+            ValueFromPipelineByPropertyName            
+        )]
+        [ValidateSet('On', 'Off')]
+        [string]$MaintenanceMode        
+    )
+    begin
+    {
+        Test-F5Session
+    }
+    process
+    {
+        foreach ($iRuleName in $Name)
+        {
+            if ($PSCmdlet.ShouldProcess("Set Maintenance Mode '$MaintenanceMode' for iRule: $iRuleName"))
+            {
+                $errorAction = $ErrorActionPreference        
+                if ($PSBoundParameters["ErrorAction"])
+                {
+                    $errorAction = $PSBoundParameters["ErrorAction"]
+                }
+
+                $iRule = Get-F5iRule -Name $iRuleName
+
+                if (-not $iRule)
+                {
+                    Write-Warning "iRule: $iRuleName was not found on F5: $($Script:F5Session.F5Name)."
+                    continue
+                }
+
+                try 
+                {
+                    $iRule.SetMaintenanceMode($MaintenanceMode, $Script:F5Session)
+                }
+                catch
+                {
+                    throw $_
+                }
+            }        
+        }        
+    }
+    end
+    {
+    }
+}
+
